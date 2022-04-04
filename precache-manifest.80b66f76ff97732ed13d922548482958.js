79de5fd8a6448a35a82213e8f883a463def1eab4
Ali asghar didari <didary7985@gmail.com>
2022-04-04 13:44:17 +0430

    ToDoList

---
+++ precache-manifest.80b66f76ff97732ed13d922548482958.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a66d4a2856b3dbb8ef",
+    "url": "/css/chunk-vendors.cbd6cc16.css"
+  },
+  {
+    "revision": "abe661e1bd87947cce9c808c742003ef",
+    "url": "/img/app_icons/icon-144x144.png"
+  },
+  {
+    "revision": "3546d64100eccb884c7d67408f2fff3c",
+    "url": "/img/app_icons/icon-152x152.png"
+  },
+  {
+    "revision": "2c12773803668f997422d08be76b4e5f",
+    "url": "/img/app_icons/icon-16x16.png"
+  },
+  {
+    "revision": "a04fcf6dce3b2d84d5049b179d43d604",
+    "url": "/img/app_icons/icon-192x192.png"
+  },
+  {
+    "revision": "68a41d063b389eaa5e145b2ce2cb46d0",
+    "url": "/img/app_icons/icon-256x256.png"
+  },
+  {
+    "revision": "1fc0472e99e6944eca280416b53b5220",
+    "url": "/img/app_icons/icon-32x32.png"
+  },
+  {
+    "revision": "3225ff89e50e28fe192482b8a06edc58",
+    "url": "/img/app_icons/icon-384x384.png"
+  },
+  {
+    "revision": "c4ab6629cefb5aa89198dec8d45e2cc7",
+    "url": "/img/app_icons/icon-512x512.png"
+  },
+  {
+    "revision": "46a47990e87f8d42eb6ebfd3290fe82f",
+    "url": "/img/app_icons/manifest.webmanifest"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ef44652fce9f0b286e9bb71f212acc15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fa9d63d982d1d8247a1",
+    "url": "/js/about.057f9cf1.js"
+  },
+  {
+    "revision": "c3a8af5d456a0a83e70f",
+    "url": "/js/app.3c3e7d48.js"
+  },
+  {
+    "revision": "62a66d4a2856b3dbb8ef",
+    "url": "/js/chunk-vendors.59c1e334.js"
+  },
+  {
+    "revision": "3176bd35197de8dff309e6df65773bbc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
